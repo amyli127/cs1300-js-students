b7e4567215ec34aaf12ed3d748f71b5d547c7602
Amy Li <amy_li1@brown.edu>
2020-10-26 18:54:28 -0400

    done

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var corsApiUrl = "https://cors-anywhere.herokuapp.com/";
 // TODO: REPLACE YOUR TOKEN
-var apiToken = "?token=YOUR_TOKEN_HERE";
+var apiToken = "?token=TvvOQacpiREHyKbOHcTL7-Gz0OrRmLzXXBAc_3usebw";
 
 // CORS stands for "cross origin resource sharing" -- you'll be making http requests in order
 // DON'T CHANGE THIS: fetches the data from the API endpoint
@@ -25,7 +25,28 @@ corsPromise().then(
   (request) =>
     (request.onload = request.onerror = function () {
       // TODO: ADD FUNCTION, ETC. FOR WHATEVER YOU WANT TO DO ONCE THE DATA IS RECEIVED
+      handleResponse(request.response);
     })
 );
 
 //// TODO: ADD WHATEVER FUN CONTENT YOU WANT ////
+const handleResponse = (response) => {
+  const jsonified = JSON.parse(response);
+  const plants = jsonified.data;
+  console.log(plants)
+  
+  const filteredPlants = plants.filter((element) => {
+    return element.family_common_name = "Grass family";
+  })
+
+  for (const plant of filteredPlants) {
+    turnPlantIntoHtml(plant);
+  }
+}
+
+const turnPlantIntoHtml = (plant) => {
+  const plantDiv = document.createElement("div");
+  plantDiv.innerText = plant.common_name + " - " + plant.scientific_name;
+  const mainContainerDiv = document.getElementById("mainContainer")
+  mainContainerDiv.appendChild(plantDiv)
+}
